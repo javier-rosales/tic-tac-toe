58938f89e99123a64703789b3577fe016a4ac447
Javier Rosales <cyjav.dev@gmail.com>
2023-08-31 16:13:20 -0600

    Add game reset button transition and handle event listeners

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,10 +1,31 @@
+const playerOne = document.querySelector("#player-one")
+const playerTwo = document.querySelector("#player-two")
+
 const gameReset = document.querySelector("#game-reset")
 const gameSettings = document.querySelector(".game-settings")
 const gameboard = document.querySelector("#gameboard")
 const textInstruction = document.querySelector(".text.instruction")
 
-gameReset.addEventListener("click", () => {
-    textInstruction.classList.toggle("removed")
-    gameSettings.classList.toggle("hidden")
-    gameboard.classList.toggle("hidden")
-})
+playerOne.addEventListener("click", startGame)
+
+gameReset.addEventListener("click", restartGame)
+
+function startGame() {
+    textInstruction.classList.add("removed")
+    gameSettings.classList.add("hidden")
+    gameboard.classList.remove("hidden")
+    gameReset.classList.remove("hidden")
+
+    playerOne.removeEventListener("click", startGame)
+    gameReset.addEventListener("click", restartGame)
+}
+
+function restartGame() {
+    textInstruction.classList.remove("removed")
+    gameSettings.classList.remove("hidden")
+    gameboard.classList.add("hidden")
+    gameReset.classList.add("hidden")
+
+    gameReset.removeEventListener("click", restartGame)
+    playerOne.addEventListener("click", startGame)
+}
